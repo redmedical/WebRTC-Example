f68e952098d23360193484fd64517a4280a553c3
jochenonline <jochenonline@googlemail.com>
2018-11-11 07:45:08 +0100

    reformat code and implement rooms

--- server/server.js
+++ server/server.js
@@ -7,24 +7,24 @@ const WebSocketServer = WebSocket.Server;
 
 // Yes, TLS is required
 const serverConfig = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
 };
 
 // ----------------------------------------------------------------------------------------
 
 // Create a server for the client html page
-const handleRequest = function(request, response) {
-  // Render the single client html file for any request the HTTP server receives
-  console.log('request received: ' + request.url);
+const handleRequest = function (request, response) {
+    // Render the single client html file for any request the HTTP server receives
+    console.log('request received: ' + request.url);
 
-  if(request.url === '/') {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(fs.readFileSync('client/index.html'));
-  } else if(request.url === '/webrtc.js') {
-    response.writeHead(200, {'Content-Type': 'application/javascript'});
-    response.end(fs.readFileSync('client/webrtc.js'));
-  }
+    if (request.url === '/') {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(fs.readFileSync('client/index.html'));
+    } else if (request.url === '/webrtc.js') {
+        response.writeHead(200, {'Content-Type': 'application/javascript'});
+        response.end(fs.readFileSync('client/webrtc.js'));
+    }
 };
 
 const httpsServer = https.createServer(serverConfig, handleRequest);
@@ -35,20 +35,21 @@ httpsServer.listen(HTTPS_PORT, '0.0.0.0');
 // Create a server for handling websocket calls
 const wss = new WebSocketServer({server: httpsServer});
 
-wss.on('connection', function(ws) {
-  ws.on('message', function(message) {
-    // Broadcast any received message to all clients
-    console.log('received: %s', message);
-    wss.broadcast(message);
-  });
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        // Broadcast any received message to all clients
+        console.log('received: %s', message);
+        wss.broadcast(message);
+    });
 });
 
-wss.broadcast = function(data) {
-  this.clients.forEach(function(client) {
-    if(client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
+wss.broadcast = function (data) {
+    this.clients.forEach(function (client) {
+        if (client.readyState === WebSocket.OPEN) {
+            console.log(data);
+            client.send(data);
+        }
+    });
 };
 
 console.log('Server running. Visit https://localhost:' + HTTPS_PORT + ' in Firefox/Chrome.\n\n\
